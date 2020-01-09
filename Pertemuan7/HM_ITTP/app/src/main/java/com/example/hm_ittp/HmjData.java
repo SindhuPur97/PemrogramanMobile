c213a24fd410c35a3b478b2d4c90d9a27612521b
SindhuPur97 <sindhupurnama52@gmail.com>
2020-01-09 09:03:15 +0700

    Pertemuan7

---
+++ Pertemuan7/HM_ITTP/app/src/main/java/com/example/hm_ittp/HmjData.java
@@ -0,0 +1,64 @@
+package com.example.hm_ittp;
+
+import java.util.ArrayList;
+
+public class HmjData {
+
+    public static String [][] data = new String[][] {
+            {"0",
+                    "Gunung Lawu",
+                    "Gunung Lawu (3.265 MDPL) terletak di Pulau Jawa, Indonesia, tepatnya di perbatasan Provinsi Jawa Tengah dan Jawa Timur. Gunung Lawu terletak di antara tiga kabupaten yaitu Kabupaten Karanganyar, Jawa Tengah, Kabupaten Ngawi, dan Kabupaten Magetan, Jawa Timur. Status gunung ini adalah gunung api \"istirahat\" (diperkirakan terahkir meletus pada tanggal 28 November 1885 dan telah lama tidak aktif, terlihat dari rapatnya vegetasi serta puncaknya yang tererosi. Di lerengnya terdapat kepundan kecil yang masih mengeluarkan uap air (fumarol) dan belerang (solfatara). Gunung Lawu mempunyai kawasan hutan Dipterokarp Bukit, hutan Dipterokarp Atas, hutan Montane, dan hutan Ericaceous. Gunung Lawu adalah sumber inspirasi dari nama kereta api Argo Lawu, kereta api eksekutif yang melayani Solo Balapan-Gambir, Gunung Lawu memiliki tiga puncak, Puncak Hargo Dalem, Hargo Dumiling dan Hargo Dumilah. Yang terakhir ini adalah puncak tertinggi.",
+                    "https://asset.kompas.com/crops/heSaiwMi47mNt9IBKYLGgR3lgMc=/0x0:780x520/750x500/data/photo/2019/06/21/3507694318.jpg"},
+            {"1",
+                    "Gunung Semeru",
+                    "Gunung Semeru atau Gunung Meru adalah sebuah gunung berapi kerucut di Jawa Timur, Indonesia. Gunung Semeru merupakan gunung tertinggi di Pulau Jawa, dengan puncaknya Mahameru, 3.676 meter dari permukaan laut (mdpl). Gunung Semeru juga merupakan gunung berapi tertinggi ketiga di Indonesia setelah Gunung Kerinci di Sumatra dan Gunung Rinjani di Nusa Tenggara Barat. Kawah di puncak Gunung Semeru dikenal dengan nama Jonggring Saloko. Gunung Semeru secara administratif termasuk dalam wilayah dua kabupaten, yakni Kabupaten Malang dan Kabupaten Lumajang, Provinsi Jawa Timur. Gunung ini termasuk dalam kawasan Taman Nasional Bromo Tengger Semeru, Semeru mempunyai kawasan hutan Dipterokarp Bukit, hutan Dipterokarp Atas, hutan Montane, dan Hutan Ericaceous atau hutan gunung.",
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Semeru.jpg/280px-Semeru.jpg"},
+            {"2",
+                    "Gunung Rinjani",
+                    "Gunung Rinjani adalah gunung yang berlokasi di Pulau Lombok, Nusa Tenggara Barat. Gunung yang merupakan gunung berapi kedua tertinggi di Indonesia dengan ketinggian 3.726 m dpl serta terletak pada lintang 8º25' LS dan 116º28' BT ini merupakan gunung favorit bagi pendaki Indonesia karena keindahan pemandangannya. Gunung ini merupakan bagian dari Taman Nasional Gunung Rinjani yang memiliki luas sekitar 41.330 ha dan ini akan diusulkan penambahannya sehingga menjadi 76.000 ha ke arah barat dan timur, Secara administratif gunung ini berada dalam wilayah tiga kabupaten: Lombok Timur, Lombok Tengah dan Lombok Barat.",
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Savanna_Mt._Rinjani_5.JPG/800px-Savanna_Mt._Rinjani_5.JPG"},
+            {"3",
+                    "Gunung Merapi",
+                    "Gunung Merapi (ketinggian puncak 2.930 mdpl, per 2010) adalah gunung berapi di bagian tengah Pulau Jawa dan merupakan salah satu gunung api teraktif di Indonesia. Lereng sisi selatan berada dalam administrasi Kabupaten Sleman, Daerah Istimewa Yogyakarta, dan sisanya berada dalam wilayah Provinsi Jawa Tengah, yaitu Kabupaten Magelang di sisi barat, Kabupaten Boyolali di sisi utara dan timur, serta Kabupaten Klaten di sisi tenggara. Kawasan hutan di sekitar puncaknya menjadi kawasan Taman Nasional Gunung Merapi sejak tahun 2004.\n" +
+                            "Gunung ini sangat berbahaya karena menurut catatan modern mengalami erupsi setiap dua sampai lima tahun sekali dan dikelilingi oleh permukiman yang sangat padat. Sejak tahun 1548, gunung ini sudah meletus sebanyak 68 kali.[butuh rujukan] Kota Magelang dan Kota Yogyakarta adalah kota besar terdekat, berjarak kurang dari 30 km dari puncaknya. ",
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Merapi_mountain.jpg/288px-Merapi_mountain.jpg"},
+            {"4",
+                    "Gunung Bromo",
+                    "Gunung Bromo (dari bahasa Sanskerta: Brahma, salah seorang Dewa Utama dalam agama Hindu) atau dalam bahasa Tengger adalah sebuah gunung berapi aktif di Jawa Timur, Indonesia. Gunung ini memiliki ketinggian 2.329 meter di atas permukaan laut dan berada dalam empat wilayah kabupaten, yakni Kabupaten Probolinggo, Kabupaten Pasuruan, Kabupaten Lumajang, dan Kabupaten Malang. Gunung Bromo terkenal sebagai objek wisata utama di Jawa Timur. Sebagai sebuah objek wisata, Bromo menjadi menarik karena statusnya sebagai gunung berapi yang masih aktif. Gunung Bromo termasuk dalam kawasan Taman Nasional Bromo Tengger Semeru.",
+                    "https://awsimages.detik.net.id/community/media/visual/2019/03/08/96d60356-f54f-4b9d-a5af-4cbc8c24f3c7_169.jpeg?w=780&q=90"},
+            {"5",
+                    "Gunung Sumbing",
+                    "Gunung Sumbing adalah gunung api yang terdapat di Jawa Tengah, Indonesia.(Ketinggian puncak 3.371 mdpl), gunung Sumbing merupakan gunung tertinggi ketiga di Pulau Jawa setelah Gunung Semeru dan Gunung Slamet. Gunung ini secara administratif terletak di tiga wilayah kabupaten, yaitu Kabupaten Magelang; Kabupaten Temanggung; dan Kabupaten Wonosobo. Bersama dengan Gunung Sindoro, Gunung Sumbing membentuk bentang alam gunung kembar, seperti Gunung Merapi dan Gunung Merbabu, apabila dilihat dari arah Temanggung. Celah antara gunung ini dan Gunung Sindoro dilalui oleh jalan provinsi yang menghubungkan kota Temanggung dan kota Wonosobo. Jalan ini biasa dijuluki sebagai Kledung Pass.",
+                    "https://cdn2.tstatic.net/jateng/foto/bank/images/gunung-sumbing-dilihat-dari-gunung-sindoro.jpg"},
+            {"6",
+                    "Gunung Agung",
+                    "Gunung Agung adalah gunung tertinggi di pulau Bali dengan ketinggian 3.031 mdpl. Gunung ini terletak di kecamatan Rendang, Kabupaten Karangasem, Bali, Indonesia. Pura Besakih, yang merupakan salah satu Pura terpenting di Bali, terletak di lereng gunung ini.\n" +
+                            "Gunung Agung adalah gunung berapi tipe stratovolcano, gunung ini memiliki kawah yang sangat besar dan sangat dalam yang kadang-kadang mengeluarkan asap dan uap air. Dari Pura Besakih gunung ini tampak dengan kerucut runcing sempurna, tetapi sebenarnya puncak gunung ini memanjang dan berakhir pada kawah yang melingkar dan lebar.\n" +
+                            "\n",
+                    "https://cdns.klimg.com/merdeka.com/i/w/news/2018/07/04/990956/670x335/gunung-agung-erupsi-ini-4-faktanya-yang-mengejutkan-gunung-agung.jpg"},
+            {"7",
+                    "Gunung Kerinci",
+                    "Gunung Kerinci (juga dieja \"Kerintji\", dan dikenal sebagai Gunung Gedang, Berapi Kurinci, Kerinchi, Korinci, atau Puncak Indrapura) adalah gunung tertinggi di Sumatra, gunung berapi tertinggi di Indonesia, dan puncak tertinggi di Indonesia di luar Papua. Gunung Kerinci terletak di Provinsi Jambi, di Pegunungan Bukit Barisan, dekat pantai barat, dan terletak sekitar 130 km sebelah selatan Padang Provinsi Sumatra Barat. Gunung ini dikelilingi hutan lebat Taman Nasional Kerinci Seblat dan merupakan habitat harimau sumatra dan badak sumatra.",
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Uprising-mount_kerinci.jpg/280px-Uprising-mount_kerinci.jpg"},
+            {"8",
+                    "Gunung Slamet",
+                    "Gunung Slamet (3.428 meter dpl.) adalah sebuah gunung berapi kerucut yang terdapat di Pulau Jawa, Indonesia. Gunung Slamet terletak di antara 5 kabupaten, yaitu Kabupaten Brebes, Kabupaten Banyumas, Kabupaten Purbalingga, Kabupaten Tegal, dan Kabupaten Pemalang, Provinsi Jawa Tengah. Gunung Slamet merupakan gunung tertinggi di Jawa Tengah serta kedua tertinggi di Pulau Jawa setelah Gunung Semeru. Kawah IV merupakan kawah terakhir yang masih aktif sampai sekarang, dan terakhir aktif hingga pada level siaga medio-2009.\n" +
+                            "Gunung Slamet cukup populer sebagai sasaran pendakian meskipun medannya dikenal sulit. Di kaki gunung ini terletak kawasan wisata Baturraden yang menjadi andalan Kabupaten Banyumas karena hanya berjarak sekitar 15 km dari Purwokerto.",
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT64L2QNx8G4ZzG5mvwLF7ErJ_RvDWX37soPHHxD-WJ60P1tY9i3g&s"}
+    };
+
+
+    public static ArrayList<Hmj> getListData(){
+        ArrayList<Hmj> list = new ArrayList<>();
+        for (String[] aData : data){
+            Hmj hmj = new Hmj();
+            hmj.setId(Integer.parseInt(aData[0]));
+            hmj.setName(aData[1]);
+            hmj.setDescription(aData[2]);
+            hmj.setPhoto(aData[3]);
+
+            list.add(hmj);
+        }
+        return list;
+    }
+}
